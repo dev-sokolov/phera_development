17c9cf1ecd3869fdafc8206c545037f61bc2810f
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-11-01 23:00:54 +0100

    fix

--- src/pages/CameraViewPage/CameraViewPage.jsx
+++ src/pages/CameraViewPage/CameraViewPage.jsx
@@ -13,6 +13,7 @@ import processing_2 from "../../assets/lottie/processing_2.json";
 import processing_3 from "../../assets/lottie/processing_3.json";
 import processing_4 from "../../assets/lottie/processing_4.json";
 import processing_5 from "../../assets/lottie/processing_5.json";
+import processing_6 from "../../assets/lottie/processing_6.json";
 // import scanning_55 from "../../assets/lottie/scanning_55.lottie";
 import cross from "../../assets/icons/cross.png";
 
@@ -42,7 +43,7 @@ const CameraViewPage = ({ onCapture, onExit }) => {
             const imageSrc = webcamRef.current?.getScreenshot();
             stopCamera();
             if (imageSrc) onCapture(imageSrc);
-        }, 1500);
+        }, 2300);
     };
 
     //     const handleCapture = async () => {
@@ -131,8 +132,8 @@ const CameraViewPage = ({ onCapture, onExit }) => {
                     style={{ opacity: isProcessing ? 0 : 1 }}
                 ></button>
 
-                <Lottie
-                    animationData={processing_5}
+                {/* <Lottie
+                    animationData={processing_6}
                     // loop={true}
                     style={{
                         width: "120px",
@@ -143,6 +144,22 @@ const CameraViewPage = ({ onCapture, onExit }) => {
                         transform: "translate(-50%, -50%)",
                         opacity: isProcessing ? 1 : 0,
                         pointerEvents: "none",
+                    }}
+                /> */}
+                <Lottie
+                    key={isProcessing ? "processing" : "idle"} // 🔑 при изменении состояния Lottie пересоздаётся
+                    animationData={processing_6}
+                    loop={false}
+                    style={{
+                        width: "80px",
+                        height: "80px",
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        opacity: isProcessing ? 1 : 0,
+                        pointerEvents: "none",
+                        filter: "brightness(0) invert(1)",
                     }}
                 />
             </div>
